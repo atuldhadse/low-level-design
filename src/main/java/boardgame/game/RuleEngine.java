83f3c7885e904c64ff889708347c3e9ac082e74b
atul-mps <atul.dhadse@highwirepress.com>
2025-08-02 01:24:54 +0530

    refactored game engine to follow -> don't repeat yourself

--- src/main/java/boardgame/game/RuleEngine.java
+++ src/main/java/boardgame/game/RuleEngine.java
@@ -10,53 +10,44 @@ public class RuleEngine {
 	public GameResult getState(Board board) {
 
 		if (board instanceof TicTacToeBoard ticTacBoard) {
-
 			GameResult result;
-
 			result = isVictory((row, col) -> ticTacBoard.getCellSymbol(row, col));
 			if (result.isOver()) {
 				return result;
 			}
-
 			result = isVictory((row, col) -> ticTacBoard.getCellSymbol(col, row));
 			if (result.isOver()) {
 				return result;
 			}
-
-			result = checkStreak(i -> ticTacBoard.getCellSymbol(i, i), i -> ticTacBoard.getCellSymbol(i / 2, i / 2));
+			result = checkStreak(i -> ticTacBoard.getCellSymbol(i, i));
 			if (result.isOver()) {
 				return result;
 			}
-
-			result = checkStreak(i -> ticTacBoard.getCellSymbol(i, 2 - i),
-					i -> ticTacBoard.getCellSymbol(i / 2, i / 2));
+			result = checkStreak(i -> ticTacBoard.getCellSymbol(i, 2 - i));
 			if (result.isOver()) {
 				return result;
 			}
-
 			return new GameResult(false, new Player("-"));
-
 		} else {
 			throw new IllegalArgumentException();
 		}
+
 	}
 
 	public GameResult isVictory(BiFunction<Integer, Integer, String> next) {
 		for (int i = 0; i < 3; i++) {
-			int j;
-			for (j = 1; j < 3 && next.apply(i, j) != null; j++) {
-				if (!next.apply(i, j).equals(next.apply(i, j - 1))) {
-					break;
-				}
-			}
-			if (j == 3) {
-				return new GameResult(true, new Player(next.apply(i, 0)));
+			int finalI = i;
+			IntFunction<String> f = j -> next.apply(finalI, j);
+			GameResult checkStreak = checkStreak(f);
+			if (checkStreak.isOver()) {
+				return checkStreak;
 			}
 		}
 		return new GameResult(false, null);
 	}
 
-	public GameResult checkStreak(IntFunction<String> point, IntFunction<String> symbolFetcher) {
+	public GameResult checkStreak(IntFunction<String> point) {
+		String firstSymbol = point.apply(0);
 		int i;
 		for (i = 1; i < 3 && point.apply(i) != null; i++) {
 			if (!point.apply(i).equals(point.apply(i - 1))) {
@@ -64,7 +55,7 @@ public class RuleEngine {
 			}
 		}
 		if (i == 3) {
-			return new GameResult(true, new Player(symbolFetcher.apply(i)));
+			return new GameResult(true, new Player(firstSymbol));
 		}
 		return new GameResult(false, null);
 	}
